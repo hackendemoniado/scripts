5729f7b6f6dff132861e1c5a10a7097fc9211e41
hackendemoniado <sergiosysforence@hotmail.com.ar>
2015-12-22 18:39:46 -0300

    Create login_rootfail.sh

---
+++ login_rootfail.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+horaactual=`date +%R --date='-1 hours'`
+mediahora=`date +%R`
+lastb -s $horaactual -t $mediahora | grep root > /tmp/loginfailsroot.txt
+cantfail=`wc -l /tmp/loginfailsroot.txt > /tmp/cantfailroot.txt`
+contador=`awk '{print $1}' /tmp/cantfailroot.txt`
+dia=`awk '{print $5}' /tmp/loginfailsroot.txt | head -n 1`
+hora=`date +"%R"`
+diahoy=`date +"%d"`
+if [ $contador -ge 3 ] && [ $dia -eq $diahoy ]; then
+	echo "Alguien esta intentando entrar demaciadas veces como root" | mutt -s "Intento de acceso a root" sergiosysforence@hotmail.com.ar
+	fechahoy=$(date +"%d-%m-%Y-%H-%M")
+	tar -zcvf backupsloginroot.$fechahoy.tar.gz /tmp/loginfailsroot.txt
+	cp backupsloginroot.$fechahoy.tar.gz /media/veracrypt1/backupsscripts/backups_loginroot
+	rm -rf backupsloginroot.$fechahoy.tar.gz
+	rm -rf /tmp/loginfailsroot.txt
+fi
